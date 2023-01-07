aed90225e154f9119938816ee89b3635a6a44f2d
Roman Vasin <rvasin@dev.vtb.ru>
2023-01-08 01:21:00 +0300

    дз на сдачу

--- src/test/java/tests/TestBase.java
+++ src/test/java/tests/TestBase.java
@@ -12,10 +12,15 @@ import org.openqa.selenium.remote.DesiredCapabilities;
 public class TestBase {
     @BeforeAll
     static void beforeAll(){
+        Configuration.browser = System.getProperty("browser", "chrome");
+        Configuration.browserSize= System.getProperty("browserSize", "1920x1080");
+        Configuration.browserVersion = System.getProperty("browserVersion", "100.0");
+        Configuration.remote = System.getProperty("remoteBrowserUrl", "https://user1:1234@selenoid.autotests.cloud")+"/wd/hub";
         Configuration.baseUrl="https://demoqa.com";
-        Configuration.browserSize="1920x1080";
+
+
     //    Configuration.remote = "https://user1:1234@selenoid.autotests.cloud/wd/hub";
-        Configuration.remote = System.getProperty("REMOTE_BROWSER_URL", "https://user1:1234@selenoid.autotests.cloud/wd/hub");
+
 
         DesiredCapabilities capabilities = new DesiredCapabilities();
         capabilities.setCapability("enableVNC",true);
